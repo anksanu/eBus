85c89bfbf193f924d1d28c494579fca6f4f42ead
Ankit Agrawal <ankit.agrawal@flipkart.com>
2016-09-10 23:29:53 +0530

    Added release builds

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+"use strict";
+
+let webpack = require('webpack');
+let PROD = JSON.parse(process.env.PROD_ENV || '0');
+
+
 module.exports = {
     entry: {
         test : "./tests/suite.js",
@@ -5,13 +11,18 @@ module.exports = {
         nodeBuild: './build.js'
     },
     output: {
-        path: __dirname + '/build',
-        filename: "[name].js"
+        path: PROD ? __dirname + '/release' : __dirname + '/build',
+        filename: PROD ? "[name].min.js" : "[name].js"
     },
     module: {
         loaders: [
             {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
         ]
     },
+    plugins: PROD ? [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: { warnings: false }
+        })
+    ] : [],
     devtool: 'source-map'
 };
